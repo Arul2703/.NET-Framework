efce73b493a5b1c5626fe11c38c3c521ae81f6d9
Arularasi <56884989+Arul2703@users.noreply.github.com>
2022-11-15 16:01:42 +0530

    CRUD Operations in SQL Server database using C#

---
+++ Database connectivity ADO.NET/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+class Program
+{
+  static void Main(string[] args){
+    string ConnectionString = @"Data Source=LAPTOP-HS0OJSCM\SQLEXPRESS;Initial Catalog=master;Integrated Security=True";
+    SqlConnection con = new SqlConnection(ConnectionString);
+    con.Open();
+    SqlCommand command = new SqlCommand("SELECT * FROM demotable;",con);
+
+    SqlDataReader dataReader = command.ExecuteReader();
+    string output = "";
+    while(dataReader.Read()){
+      output = output+dataReader.GetValue(0)+"-"+dataReader.GetValue(1)+"\n";
+      Console.WriteLine(output);
+    }
+    dataReader.Close();
+    command.Dispose();
+    con.Close();
+  }
+}
